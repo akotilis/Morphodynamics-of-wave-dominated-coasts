8a4127c72415f27be9c5428877aa59c3749de71e
Smolders,E.J.V. (Emma) <e.j.v.smolders@students.uu.nl>
2021-02-24 11:48:27 +0100

    Update TheoreticalCaseBJ.m

--- Chapter4/TheoreticalCaseBJ.m
+++ Chapter4/TheoreticalCaseBJ.m
@@ -28,18 +28,19 @@ Zeta = 0;       % Mean water level (m)
 hmin = 0.2;     % Minimal water depth for computation
                 % (we stop the computation when h<hmin)
 
-%------------------------------------------------
-%       Computation of wave characteristics
-%------------------------------------------------
+%----------------------------------------------------------------------------------
+%       Computation and visualisation of wave characteristics using a variable zeta
+%----------------------------------------------------------------------------------
 %waves = BJmodelEmma(Hrms0,T0,Zeta,theta0,profile,hmin);
 
 Zeta = [-1 0 1]
+Title = ["Zeta = -1m", "Zeta = 0m", "Zeta = 1m"];
 
 for i=1:length(Zeta);
     waves(i) = BJmodelEmma(Hrms0,T0,Zeta(i),theta0,profile,hmin);
     figure(i);
     subplot(5,1,1); plot(waves(i).x,waves(i).Hrms)
-    title('title')
+    title(Title(i))
     ylabel('Hrms (m)')
     xlim([0,500]);
     subplot(5,1,2); plot(waves(i).x,waves(i).z,'k')
@@ -59,8 +60,35 @@ for i=1:length(Zeta);
 end 
 
 
+%----------------------------------------------------------------------------------
+%       Computation and visualisation of wave characteristics using a
+%       variable Hrms
+%----------------------------------------------------------------------------------
 
 
-%------------------------------------
-%           Visualisation 
-%------------------------------------
+Zeta = 0
+Hrms0 = [0.5, 2]
+Title = ["Hrms0 = 0.5m", "Hrms0 = 2m"];
+
+for i=1:length(Hrms0);
+    waves(i) = BJmodelEmma(Hrms0(i),T0,Zeta,theta0,profile,hmin);
+    figure(i);
+    subplot(5,1,1); plot(waves(i).x,waves(i).Hrms)
+    title(Title(i))
+    ylabel('Hrms (m)')
+    xlim([0,500]);
+    subplot(5,1,2); plot(waves(i).x,waves(i).z,'k')
+    hold on; plot(waves(i).x,Zeta*ones(size(x)),'-.')
+    ylabel('zb (m)')
+    xlim([0,500]);
+    subplot(5,1,3); plot(waves(i).x,waves(i).Dbr)
+    ylabel('D_{Br} (W/m^2)')
+    xlim([0,500]);
+    subplot(5,1,4); plot(waves(i).x,waves(i).Dr)
+    xlim([0,500]);
+    ylabel('D_r (W/m^2)')
+    subplot(5,1,5); plot(waves(i).x,waves(i).eta)
+    xlim([0,500]);
+    xlabel('x (m)')
+    ylabel('eta (m)')
+end 
