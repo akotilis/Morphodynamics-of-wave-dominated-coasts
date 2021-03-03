fb53cce281afe4a7ad650c13404ed1aedf08725e
mdebotton <75377849+mdebotton@users.noreply.github.com>
2021-03-03 17:32:28 +0100

    Updated code --> see changes in longshoreCurrent.m too

--- Chapter5/Currents_Mariana.m
+++ Chapter5/Currents_Mariana.m
@@ -52,19 +52,25 @@ wcross = [8.86 8.55 1.16]; %m/s
 
 dzetady = [-7.33e-6 -1.76e-5 8.57e-7]; 
 
+%We load the observations for the current for Egmond 
 currents_data = load('vEgmond.txt'); 
 
+%This are the position of the sensors for the current data 
 position_sensors = [4765 4790 4814 4835 4860 4889]; %meters 
 
+%We load the statistics from Chapter 1 
 load('StatisticsEgmond.mat'); 
 
 names = {'low tide', 'mid tide', 'high tide'}; 
 
+%We iterate for each one of the tides 
 for ii = 1:length(theta) 
-     
+
+%We calculate the wave properties over the profile for each tide 
 waves = BJmodelEmma(Hrms0(ii),T0(ii),Zeta(ii),theta(ii),profile,hmin);
 
 
+%We calculate the current for each one of the tides 
 currents(:,ii) = longshoreCurrent(profile,dzetady(ii),wcross(ii),wlong(ii),waves.c,waves.theta,waves.Dr,waves.ht,waves.st,ka,nu,0);
 
 %Interpolation of the current profile to the position of the sensors 
@@ -109,6 +115,11 @@ end
 %% Part 5.3: Complementary analysis 
 
 %All analysis is for low tide only
+
+%We added an if statement in the function longshoreCurrent.m to run for 
+%the different cases that the question asks for, defining an input type,
+%and depending on the number is the forcings it takes into account for the
+%F calculation 
 
 for ii = 1:3 
     waves = BJmodelEmma(Hrms0(1),T0(1),Zeta(1),theta(1),profile,hmin);
@@ -155,7 +166,7 @@ plot(waves_normal.x,curr_normalwaves);
 grid on; 
 xlabel('x (m)'); 
 ylabel('v (m/s)'); 
-%legend('Theta = -36', 'Theta = 0'); 
+legend('Theta = -36', 'Theta = 0'); 
 
 
 
