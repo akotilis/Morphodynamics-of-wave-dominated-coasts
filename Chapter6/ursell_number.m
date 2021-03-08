dface87d71dd35c69dd1f13ad00dd5228c7ae5ad
mdebotton <75377849+mdebotton@users.noreply.github.com>
2021-03-08 10:52:04 +0100

    profile txt

--- Chapter6/ursell_number.m
+++ Chapter6/ursell_number.m
@@ -3,17 +3,17 @@ function ur = ursell_number(k,h,Hrms)
 %ursell_number calculates the Ursell number defined as Ur = 3ak/4(kh)ˆ3
 
 %INPUT 
-%k ---> wave number 
-%Hrms ---> Root mean square wave height 
-%h ---> 
+%k ---> wavenumber 
+%Hrms ---> Root mean square wave height (meters) 
+%h ---> water depth (meters) 
 
 %OUTPUT 
 %ur ---> ursell number 
 
 
-amplitude = 0.5*sqrt(2)*Hrms; 
+amplitude = 0.5*sqrt(2).*Hrms; 
 
-ursell = 3*amplitude*k/(4*(k*h).^3); 
+ursell = 3*amplitude.*k./(4*(k.*h).^3); 
 
 ur = ursell; 
 
